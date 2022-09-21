615ba6e14545cbd16989614fad71f403b814101b
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-09-21 08:42:34 -0400

    commit tree 2b038072570af98f87e4242e72f2d383fef34feb

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
-/* Your Code Here */
+function createEmployeeRecord(employee) {
+    //[firstName, familyName, title, payPerHour] = employee
+      return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+      }
+    }
+    
+    function createEmployeeRecords(employees) {
+      return employees.map(employee => createEmployeeRecord(employee))
+    }
+    
+    function createTimeInEvent(event) {
+      let [date, hour] = event.split(" ")
+      let eventObj = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+      }
+       this.timeInEvents.push(eventObj)
+       return this
+    }
+    
+    function createTimeOutEvent(event) {
+      let [date, hour] = event.split(" ")
+      let eventObj = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date 
+      }
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+      this.timeOutEvents.push(eventObj)
+      return this
+    }
+    
+    function hoursWorkedOnDate(date) {
+      const timeIn = this.timeInEvents.find(event => event.date === date)
+      const timeOut = this.timeOutEvents.find(event => event.date === date)
+      return (timeOut.hour - timeIn.hour)/100
+    }
+    
+    function wagesEarnedOnDate(date) {
+      const hours = hoursWorkedOnDate.call(this, date)
+      return this.payPerHour * hours
+    }
+    
+    
+    
+    function calculatePayroll(employeeRecords) {
+      const record = employeeRecords.map(employee => allWagesFor.call(employee))
+      return record.reduce((currentValue, total) => currentValue + total)
+    }
+    
+    function findEmployeeByFirstName(employees, firstNameString) {
+        return employees.find(emp => emp.firstName === firstNameString)
+    }
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
